8f268e28cf7b923d241603a8114ade9f70708145
JAZWii <jazwii@yahoo.com>
2018-04-23 16:41:49 +0300

    first trial for imported project from github

--- JavaRestTest/src/main/java/com/jazwii/java_rest_test/classes/User.java
+++ JavaRestTest/src/main/java/com/jazwii/java_rest_test/classes/User.java
@@ -1,7 +1,59 @@
 package com.jazwii.java_rest_test.classes;
 
+import java.util.Date;
+
 public class User {
     private int id ;
     private String firstName;
-    
+    private String lastName;
+    private Date birthday;
+
+    public User(int id, String firstName, String lastName, Date birthday) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = birthday;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", birthday=" + birthday +
+                '}';
+    }
 }
